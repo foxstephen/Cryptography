6e0fa5f196d5ebca3bb44d046931d8f30f1c4026
Stephen Fox <stephenfox995@hotmail.com>
2016-11-17 00:12:02 +0000

    Lab ten

--- LabTen/LabTen.py
+++ LabTen/LabTen.py
@@ -10,45 +10,61 @@ image = Image.open("doggy.jpg")
 
 
 # Part 2
-# print "Enter message"
-# message = raw_input()
-
-width, height = image.size
-
-originalPixels = []
-control = []
-stegoPixels = []
-
- 
-messageCounter = {
-  chars
-  char: '',
-  index: 2 # Start from index two as each bit is 0b10101011
+# Hashes of before and after image has had message hidden.,
+hashes = {
+  "originalPixels": [],
+  "stegoPixels": []
 }
 
-for x in range(0, width):
-  for y in range(0, height):
-    pixel = image.getpixel((x, y)) # Keep original pixels
-    originalPixels.append(pixel)
-    control.append(pixel)
 
-    pixelString = str(bin(pixel[0]))
-    lsb = str(bin(ord('s')))[2] # The lsb for the current pixel
+# hides a message in a greyscale image
+def hide(image, message):
+  width, height = image.size
 
-    # Pixel with lsb set to the correct bit of the message
-    pixelWithLsb = pixelString[0:-1] + lsb
+  # Each char of the message in binary with the '0b' part removed
+  messageCharBits = map(lambda c: bin(ord(c))[2:], message)
+  # Get all the bits that make up the message.
+  messageBits = reduce(lambda prev, curr: prev + list(curr), messageCharBits, [])
 
-    # Convert the bit string with the correct lsb back to binary. 
-    r = bitstring.Bits(pixelWithLsb)
-    pixel = (r.uint, r.uint, r.uint)
-    image.putpixel((x, y), pixel)
+  # Number of bits in the message
+  messageBitCount = len(messageBits) 
+  messageBitIndex = 0
 
-    stegoPixels.append(pixel) # Pixels with lsb changed.
+  for x in range(0, width):
+    for y in range(0, height):
+      pixel = image.getpixel((x, y)) # Keep original pixels
+      hashes["originalPixels"].append(pixel)
 
-      
-  
+      # Check if image has rgb values or is just white values.
+      pixelToChange = pixel[0] if type(pixel) is tuple else pixel
+
+      pixelAsBinary = bin(pixelToChange)
+      if (messageBitIndex < messageBitCount):
+        lsb = messageBits[messageBitIndex] # The new lsb to set on the current pixel from the message
+        # Pixel with lsb set to the correct bit of the message
+        pixelWithLsbSet = pixelAsBinary[0:-1] + lsb
+        # Convert the binary string with the correct lsb back to binary. 
+        r = bitstring.Bits(pixelWithLsbSet)
+
+        # Check is origian pixel is rgb or white, and set the new pixel accordingly
+        pixel = (r.uint, r.uint, r.uint) if type(pixel) is tuple else (r.uint) # new rgb or white value with with lsb set.
+        image.putpixel((x, y), pixel)
+
+        hashes["stegoPixels"].append(pixel) # Pixels with lsb changed.
+        messageBitIndex += 1
+      else: # Weve written the message no need to continue changing pixels.
+        return
+
+print "Enter message: "
+message = raw_input()
+hide(image, message)
+
 image.save("out.jpg")
+
 # hash outputs to show difference in original vs image with message.
-print "Hash of original image" + hashlib.sha224(repr(originalPixels).encode('utf-8')).hexdigest()
-print hashlib.sha224(repr(control).encode('utf-8')).hexdigest()
-print "Hash of image with message encoded " + hashlib.sha224(repr(stegoPixels).encode('utf-8')).hexdigest()
+original = repr(hashes["originalPixels"])
+hiddenMessage = repr(hashes["stegoPixels"])
+print "original image hash :" + hashlib.sha224(original.encode('utf-8')).hexdigest()
+print "hidden message hash :" + hashlib.sha224(hiddenMessage.encode('utf-8')).hexdigest()
+
+print "Message hidden successfully in image!"
